4abf3db7a032af6f81f7c7956ee64591fa9f2ad4
soumya guchhait <soumoguchhait8587@gmail.com>
2024-03-09 14:13:01 +0530

    fixed port errors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const cors=require("cors");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
+require("dotenv").config();
 const app=express();
 const notesRouter=require("./Routes/notesRouter")
 const userRoutes=require(`./Routes/userRoutes`)
@@ -11,8 +12,11 @@ app.use(express.json({ extended: true, limit: "5mb" }));
 app.use(cors());
 app.use('/notes',notesRouter);
 app.use('/user',userRoutes);
-const Port=5000;
-const DB_URL="mongodb+srv://soumya:Guchhait8587@soumyadb.wdru3eu.mongodb.net/?retryWrites=true&w=majority&appName=soumyadb";
+const Port=process.env.PORT;
+
+const DB_URL=process.env.DB_Url;
+
+
 mongoose
   .connect(DB_URL)
   .then(() => {
@@ -22,4 +26,5 @@ mongoose
   })
   .catch((err) => {
     console.log(err.message);
+    console.log(Port)
   });
